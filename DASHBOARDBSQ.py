6246c08d55ac78e307f5b135d07e528593823bcb
Sherry Andiyani <106478515+scarlet-laurent@users.noreply.github.com>
2026-06-29 00:36:16 +0700

    Update DASHBOARDBSQ.py

--- DASHBOARDBSQ.py
+++ DASHBOARDBSQ.py
@@ -1,1 +1,23 @@
+import pandas as pd
+import streamlit as st
+import altair as alt
+import numpy as np
+import matplotlib.pyplot as plt
+from datetime import datetime
+from io import BytesIO
 
+# Set judul dan ikon pada browser
+st.set_page_config(
+    page_title='BSQ BCA KCU GADING SERPONG',
+    page_icon='📈',
+    layout='wide'
+)
+
+# Sidebar logo (tampilan lebih proporsional)
+st.sidebar.image(logo.png', width=150)
+
+# Judul
+st.title("📊 Dashboard Analisis - BSQ BCA KCU GADING SERPONG")
+
+# Upload file Excel
+uploaded_file = st.file_uploader("Unggah file Excel", type=["xlsx", "xls"])
